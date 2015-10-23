169c09dbd39e8d070603121cece0d2116b3ea89c
Nicholas Randazzo <cubeqtrader@gmail.com>
2015-10-23 15:54:48 -0400

    add server listen index.js

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.get('/', function(req, res){
 
 
 })
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
